34936a5cd558f5e1e5302a92fe741e2ac2501ff0
Carolina <carolina.silvasoares22@gmail.com>
2024-05-02 08:46:34 -0300

    teste

---
+++ heranca_polimorfismo/src/heranca_polimorfismo/Pessoa.java
@@ -0,0 +1,17 @@
+package heranca_polimorfismo;
+class Pessoa {
+    private String nome;
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+	public void imprimirDetalhes() {
+		// TODO Auto-generated method stub
+		
+	
+	}}
---
+++ heranca_polimorfismo/src/heranca_polimorfismo/PessoaFisica.java
@@ -0,0 +1,18 @@
+package heranca_polimorfismo;
+
+
+class PessoaFisica extends Pessoa {
+    private String CPF;
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getCPF() {
+        return this.CPF;
+    }
+  
+        public void imprimirDetalhes() {
+            System.out.println("Pessoa Física - Nome: " + getNome() + " - CPF: " + getCPF());
+        }
+    }
